a2731bd9aa046d933f3143a35cb4006442a3d428
patrick <me@patrickphat.com>
2021-03-11 13:47:55 +0000

    test

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='fgit',
+     version='0.0.1b0',
+     author="Truong-Phat Nguyen",
+     author_email="me@patrickphat.com",
+     description="FGIT: No longer wait for another git commit",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/patrick/fgit",
+     packages=setuptools.find_packages(exclude=['docs', 'tests', 'experiments']),
+     entry_points ={ 
+            'console_scripts': [ 
+                'fgit = cli.main:main'
+            ]
+     },
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     python_requires='>3.6',
+     install_requires =[
+         "GitPython",
+         ],
+     extras_require={
+         'dev': [
+             'pytest',
+             'coverage',
+             ],
+     }
+ )
